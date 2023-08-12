2b7608ec1e8564c3556c026866550a6602dd3095
Yuga Samuel <yugasamuel2002@gmail.com>
2023-08-12 08:41:19 +0700

    First commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Yuga Samuel on 12/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
