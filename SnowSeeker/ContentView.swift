af433a58e5e6aba33d14193baf44cee97d6b759a
Yuga Samuel <yugasamuel2002@gmail.com>
2023-08-12 10:15:14 +0700

    Add: Bundle decodable file to decode json data to model Update: Content view to have list of resorts

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List(resorts) { resort in
+                NavigationLink(value: resort) {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 25)
+                        .clipShape(
+                            RoundedRectangle(cornerRadius: 5)
+                        )
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.black, lineWidth: 1)
+                        )
+                        .padding(.trailing, 5)
+                    
+                    VStack(alignment: .leading) {
+                        Text(resort.name)
+                            .font(.headline)
+                        Text("\(resort.runs) runs")
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationTitle("Resorts")
+            .navigationDestination(for: Resort.self) { resort in
+                Text(resort.name)
+            }
         }
-        .padding()
     }
 }
 
